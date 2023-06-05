d2d20f6b5ede88dfe50b694e82e39571c345bbef
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-06-05 13:45:17 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include "lib.h"
+bool cerca(char lista_nomi[10][20], char a[], int &pos){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            if(lista_nomi[i][j]!=a[j]){
+                j=20;
+            }
+            else{
+                if(j==19){
+                    pos=i;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
