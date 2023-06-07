7fd9d7fe613bcb5a9f3b3228b233e94e94b7dff5
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-06-07 19:21:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,23 +3,26 @@
 using namespace std;
 
 int main() {
-    char lista_nomi[10][20], a[20];
-    int p=0;
+char lista_nomi[10][20];
+    char a[20];
+    int pos = 0;
+
     for(int i=0;i<10;i++){
         for(int j=0;j<20;j++) {
-            lista_nomi[i][j]=' ';
-            a[j]=' ';
+            a[j] = ' ';
+            lista_nomi[i][j] = ' ';
         }
     }
+
     for(int i=0;i<10;i++){
         cin >> lista_nomi[i];
     }
+
     cin >> a;
-    if(cerca(lista_nomi, a, p)){
-        cout << p;
-    }
-    else{
-        cout << "non trovato\n";
+    if(ricerca_nome(lista_nomi,pos,a)){
+        cout << pos <<endl;
+    }else{
+        cout << "non trovato";
     }
     return 0;
 }
