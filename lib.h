08e3ff2520247aeaf183154e4ead89ef3050ce41
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-06-08 22:05:37 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,5 @@
-bool ricerca_nome(char[10][20], int &, char[]);
+void init (char mat[], int n);
+void init(char mat[5][15]);
+void inserimento(char nomi[5][15], char cognomi[5][15]);
+void inserimento(char mat[], int nomi);
+int ricerca(char nomi[5][15], char cognomi[5][15], char nome[15], char cognome[15]);
